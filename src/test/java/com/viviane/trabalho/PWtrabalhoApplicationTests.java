a5379d03b7f20f8df080fb0566da7e2d499ead33
VivianePecenin <vivipece555@gmail.com>
2023-06-21 22:16:30 -0300

    Trabalho G2 PWII

---
+++ src/test/java/com/viviane/trabalho/PWtrabalhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viviane.trabalho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PWtrabalhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
